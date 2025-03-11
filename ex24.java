6119e91b33cc052306501ae418b929cf5eb34221
lpelisser <luanopelisser@gmail.com>
2025-03-11 11:36:09 -0300

    Add files via upload

---
+++ ex24.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ex24 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        double multiplicacao,soma;
+        int n1,n2,n3,n4;
+
+        System.out.println("Escreva o primeiro numero inteiro: ");
+        n1 = entrada.nextInt();
+
+        System.out.println("Escreva o segundo numero inteiro: ");
+        n2 = entrada.nextInt();
+
+        System.out.println("Escreva o terceiro numero inteiro: ");
+        n3 = entrada.nextInt();
+
+        System.out.println("Escreva o quarto numero inteiro: ");
+        n4 = entrada.nextInt();
+
+
+
+
+        
+        entrada.close();
+    }
+    
+}
